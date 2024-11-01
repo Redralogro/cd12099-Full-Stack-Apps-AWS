73dfe89913cbfd6f97e5dbf557807ca78dd455d5
Long Duc Vu <redralogro@gmailcom>
2024-11-01 15:08:54 +0700

    Create endpoint to get image from URL

--- project starter code/server.js
+++ project starter code/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
+import {isUri}  from 'valid-url';
 
 
 
+(async ()=>{
   // Init the Express application
   const app = express();
 
@@ -31,6 +33,34 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
 
   //! END @TODO1
   
+  // Endpoint to filter an image from a public URL
+  app.get('/filteredimage', async (req, res) => {
+    const { image_url } = req.query;
+
+    // 1. Validate the image_url query
+    if (!image_url) {
+      return res.status(400).send({ message: 'image_url is required' });
+    }
+    if(!isUri(image_url)){
+      return res.status(422).send({ message: 'Not a valid URL' });
+    }
+    
+    try {
+      // 2. Call filterImageFromURL(image_url) to filter the image
+      const filteredPath = await filterImageFromURL(image_url);
+      // 3. Send the resulting file in the response
+      res.sendFile(filteredPath, async (err) => {
+        if (err) {
+          console.error("Error sending file:", err);
+        } else {
+          // 4. Delete any files on the server after response is sent
+          await deleteLocalFiles([filteredPath]);
+        }
+      });
+    } catch (error) {
+      res.status(500).send({ message: `Error processing image: ${error}` });
+    }
+  });
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async (req, res) => {
@@ -43,3 +73,5 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
       console.log( `server running http://localhost:${ port }` );
       console.log( `press CTRL+C to stop server` );
   } );
+
+})();
